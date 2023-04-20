f38427c7f3ff8dcad1bb10c1021b2f0b8ece3d64
MrFocusPocus <proyectos@marcogonzalez.cl>
2023-04-19 23:08:01 -0400

    primer commit

---
+++ TEST/src/components/Producto.jsx
@@ -0,0 +1,16 @@
+export const Producto = (props) => {
+    console.log(props)
+
+    return (
+        <ul>
+            <li>
+                Nombre: Producto 1
+            </li>
+            <li>
+                Precio: $10.000
+            </li>
+        </ul>
+    )
+}
+
+export default Producto
